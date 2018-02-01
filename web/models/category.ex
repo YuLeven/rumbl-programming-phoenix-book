82a6602a2e5f162413835b0b3de50812bac18161
Yuri Levenhagen <yurileven@gmail.com>
2018-02-01 16:27:40 -0200

    Added functions to provide readily available common criteria to category queries

--- web/models/category.ex
+++ web/models/category.ex
@@ -15,4 +15,18 @@ defmodule Rumbl.Category do
     |> cast(params, [:name])
     |> validate_required([:name])
   end
+
+  @doc """
+  Appends criteria to order the categories alphabetically to a query
+  """
+  def alphabetical(query) do
+    fro c in query, order_by: c.name
+  end
+
+  @doc """
+  Appends criteria to limit the result to the category's name and id
+  """
+  def names_and_ids(query) do
+    from c in query, select: {c.name, c.id}
+  end
 end
