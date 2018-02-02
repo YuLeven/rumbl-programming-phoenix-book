9e8e1ec6f585b9cfb125e52f4d6c62d384bcde4a
Yuri Levenhagen <yurileven@gmail.com>
2018-02-02 10:54:23 -0200

    Moved back video authentication tests to video_controller_test to experiment with tags

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -5,12 +5,34 @@ defmodule Rumbl.VideoControllerTest do
   @valid_attrs %{description: "some content", title: "some content", url: "some content"}
   @invalid_attrs %{}
   
-  setup do
-    user = insert_user(username: "Larry")
-    conn = assign(build_conn(), :current_user, user)
-    {:ok, conn: conn, user: user}
+  setup  %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(build_conn(), :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
   end
 
+  test "requires user authentication on all actions", %{conn: conn} do
+    Enum.each(
+      [
+        get(conn, video_path(conn, :new)),
+        get(conn, video_path(conn, :index)),
+        get(conn, video_path(conn, :show, "123")),
+        get(conn, video_path(conn, :edit, "123")),
+        get(conn, video_path(conn, :update, "123", %{})),
+        get(conn, video_path(conn, :create, %{})),
+        get(conn, video_path(conn, :delete, "123"))
+      ], fn conn ->
+        assert html_response(conn, 302)
+        assert conn.halted
+      end
+    )
+  end
+
+  @tag login_as: "Larry"
   test "lists all entries on index", %{conn: conn, user: user} do
     user_video = insert_video(user, title: "Chunky Bacon")
     other_user_video = insert_video(insert_user(username: "Charles"), title: "Smoky Bacon")
