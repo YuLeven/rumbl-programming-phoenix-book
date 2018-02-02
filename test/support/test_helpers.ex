db11f752adc690268a578d7d0f5e6db532c8e3be
Yuri Levenhagen <yurileven@gmail.com>
2018-02-02 13:35:03 -0200

    Replaced deprecated function Dict.merge() with suitable alternative

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -5,11 +5,13 @@ defmodule Rumbl.TestHelpers do
     Inserts a mock user to be used throughout the tests
     """
     def insert_user(attrs \\ %{}) do
-        changes = Dict.merge(%{
-            name: "Some user",
-            username: "user#{Base.encode16(:crypto.strong_rand_bytes(8))}",
-            password: "canttouchthis"
-        }, attrs)
+        changes = 
+            attrs
+            |> Enum.into(%{
+                name: "Some user",
+                username: "user#{Base.encode16(:crypto.strong_rand_bytes(8))}",
+                password: "canttouchthis"
+            })
 
         %Rumbl.User{}
         |> Rumbl.User.registration_changeset(changes)
