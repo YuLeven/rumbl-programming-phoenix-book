c88c3012dc047c6fef4e87409008db2e300fdf0d
Yuri Levenhagen <yurileven@gmail.com>
2018-02-03 09:12:46 -0200

    Scaffolded structures to handle watching videos

---
+++ web/views/watch_view.ex
@@ -0,0 +1,12 @@
+defmodule Rumbl.WatchView do
+    use Rumbl.Web, :view
+
+    @doc """
+    Extracts the video ID from a youtube video
+    """
+    def player_id(video) do
+        ~r{^.*(​?:​youtu\.be/|\w+/|v=)(​?<​id>[^​#&?]*)}
+        |> Regex.named_captures(video.url)
+        |> get_in(["id"])
+    end
+end
