3f6af5c817ca80e93ca95e100c98802fc79c172e
Yuri Levenhagen <yurileven@gmail.com>
2018-02-04 21:50:29 -0200

    The regex in this view failed to account for differences in the URL (youtube.com vs youtu.be)

--- web/views/watch_view.ex
+++ web/views/watch_view.ex
@@ -5,7 +5,7 @@ defmodule Rumbl.WatchView do
     Extracts the video ID from a youtube video
     """
     def player_id(video) do
-        ~r{^.*(​?:​youtu\.be/|\w+/|v=)(​?<​id>[^​#&?]*)}
+        ~r{^.*(?:youtu\.?be/|w+/|v=)(?<id>[^#&?]*)}
         |> Regex.named_captures(video.url)
         |> get_in(["id"])
     end
