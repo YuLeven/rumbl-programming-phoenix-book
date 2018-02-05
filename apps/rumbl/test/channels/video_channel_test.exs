23228306725725fb5396d86306a04627a0eb9815
Yuri Levenhagen <yurileven@gmail.com>
2018-02-05 17:42:08 -0200

    Added an integration test for publishing a new wolfram post

--- apps/rumbl/test/channels/video_channel_test.exs
+++ apps/rumbl/test/channels/video_channel_test.exs
@@ -31,4 +31,14 @@ defmodule Rumbl.Channels.VideoChannelTest do
         assert_reply ref, :ok, %{}
         assert_broadcast "new_annotation", %{}
     end
+
+    test "new annotations trigger InfoSys", %{socket: socket, video: video} do
+        insert_user(username: "wolfram")
+        {:ok, _, socket} = subscribe_and_join(socket, "videos:#{video.id}", %{})
+        ref = push socket, "new_annotation", %{body: "1 + 1", at: 123}
+
+        assert_reply ref, :ok, %{}
+        assert_broadcast "new_annotation", %{body: "1 + 1", at: 123}
+        assert_broadcast "new_annotation", %{body: "2", at: 123}
+    end
 end
