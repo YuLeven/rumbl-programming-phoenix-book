c9ecc2f18f7fbd9b5aee997a8bd096e97084f776
Yuri Levenhagen <yurileven@gmail.com>
2018-02-05 15:23:53 -0200

    Finished implementing the wolfram backend

--- lib/rumbl/info_sys/info_sys.ex
+++ lib/rumbl/info_sys/info_sys.ex
@@ -9,6 +9,9 @@ defmodule Rumbl.InfoSys do
         backend.start_link(query, query_ref, owner, limit)
     end
 
+    @doc """
+    Computers the given query, returning relevant information from the registered backend services
+    """
     def compute(query, opts \\ []) do
         limit = opts[:limit] || 10
         backends = opts[:backends] || @backends
@@ -29,8 +32,12 @@ defmodule Rumbl.InfoSys do
         {pid, monitor_ref, query_ref}
     end
 
-    defp await_results(children, _opts) do
-        await_result(children, [], :infinity)
+    defp await_results(children, opts) do
+        timeout = opts[:timeout] || 5000
+        timer = Process.send_after(self(), :timedout, timeout)
+        results = await_result(children, [], :infinity)
+        cleanup(timer)
+        results
     end
 
     defp await_result([head|tail], acc, timeout) do
@@ -40,12 +47,35 @@ defmodule Rumbl.InfoSys do
             {:results, ^query_ref, results} ->
                 Process.demonitor(monitor_ref, [:flush])
                 await_result(tail, results ++ acc, timeout)
+            
             {:DOWN, ^monitor_ref, :process, ^pid, _reason} ->
                 await_result(tail, acc, timeout)
+
+            :timedout ->
+                kill(pid, monitor_ref)
+                await_result(tail, acc, 0)
+        after
+            timeout ->
+                kill(pid, monitor_ref)
+                await_result(tail, acc, 0)
         end
     end
 
     defp await_result([], acc, _) do
         acc
+    end
+
+    defp kill(pid, ref) do
+        Process.demonitor(ref, [:flush])
+        Process.exit(pid, :kill)
+    end
+
+    defp cleanup(timer) do
+        :erlang.cancel_timer(timer)
+        receive do
+            :timedout -> :ok
+        after
+            0 -> :ok
+        end
     end
 end
