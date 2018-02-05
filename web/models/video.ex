8f4bf9672f53570612ab872ce050366f8f52e992
Yuri Levenhagen <yurileven@gmail.com>
2018-02-04 22:39:19 -0200

    Added a feature to load videos by their slug instead of an arbitrary integer

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,6 +1,8 @@
 defmodule Rumbl.Video do
   use Rumbl.Web, :model
 
+  @primary_key {:id, Rumbl.Permalink, autgenerate: true}
+
   schema "videos" do
     field :url, :string
     field :title, :string
@@ -18,7 +20,31 @@ defmodule Rumbl.Video do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:url, :title, :description, :category_id])
+    |> slugify_title()
     |> validate_required([:url, :title, :description])
     |> assoc_constraint(:category)
+  end
+
+  # We override - I have no idea of how to call this in a functional programming language.
+  # Is it the same? Anyway, we 'override' the Phoenix.Param implementation for this model
+  # so params using the view 'link' macro matches our slug schema
+  defimpl Phoenix.Param, for: Rumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+      |> String.downcase()
+      |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
