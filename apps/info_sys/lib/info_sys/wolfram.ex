ab396df55c7a2c7572dd30b662216bf66c77bc54
Yuri Levenhagen <yurileven@gmail.com>
2018-02-05 17:24:37 -0200

    Added tests for the Wolfram backend (using a stub HTTP client)

--- apps/info_sys/lib/info_sys/wolfram.ex
+++ apps/info_sys/lib/info_sys/wolfram.ex
@@ -24,12 +24,13 @@ defmodule InfoSys.Wolfram do
         send(owner, {:results, query_ref, results})
     end
 
+    @http Application.get_env(:info_sys, :wolfram)[:http_client] || :httpc
     defp fetch_xml(query_str) do
         location = String.to_charlist("http://api.wolframalpha.com/v2/query" <>
         "?appid=#{app_id()}" <>
         "&input=#{URI.encode(query_str)}" <>
         "&format=plaintext")
-        {:ok, {_, _, body}} = :httpc.request(location)
+        {:ok, {_, _, body}} = @http.request(location)
         body
     end
 
